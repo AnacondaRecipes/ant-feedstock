74cfb82ddf6f4c03bedc411d9c90a75c33063c33
Serhii Kupriienko <skupriienko@anaconda.com>
2026-05-29 10:39:23 +0000

    Update ant to 1.10.16
    Align with conda-forge apache-ant 1.10.16: tar.gz source, openjdk >=8,
    osx install-lite permission workaround, and ant -help/-version tests.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -15,4 +15,4 @@ mkdir %PREFIX%\etc\conda\activate.d
 echo set "ANT_HOME_CONDA_BACKUP=%%ANT_HOME%%" > "%PREFIX%\etc\conda\activate.d\ant_home.bat"
 echo set "ANT_HOME=%%CONDA_PREFIX%%\Library" >> "%PREFIX%\etc\conda\activate.d\ant_home.bat"
 mkdir %PREFIX%\etc\conda\deactivate.d
-echo set "ANT_HOME=%%ANT_HOME_CONDA_BACKUP%%" > "%PREFIX%\etc\conda\deactivate.d\ant_home.bat
+echo set "ANT_HOME=%%ANT_HOME_CONDA_BACKUP%%" > "%PREFIX%\etc\conda\deactivate.d\ant_home.bat"
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# export JAVA_HOME="${PREFIX}"
+set -ex
+
 export ANT_HOME="${PREFIX}"
 
 for i in etc lib bin; do
   mkdir -p "${PREFIX}/$i"
 done
 
-./build.sh install-lite
+if [[ "${target_platform}" == linux-* ]]; then
+  ./build.sh install-lite
+else
+  # on osx, the build fails with chmod/permission errors without this
+  # option, which has been renamed in source, but not in the docs, see
+  # https://github.com/apache/ant/commit/c7d9b17b080fc5250fc05144322ecc3eef8a6e87
+  ./build.sh install-lite -Dsetpermissions.fail=false
+fi
 
 # ensure that ANT_HOME is set correctly
 mkdir -p $PREFIX/etc/conda/activate.d
